e345c910b9b0455b2761a1abdc3aacd64a18e4bb
Theonlyhamstertoh <theonlyhamstertoh@gmail.com>
2021-04-11 12:44:03 -0700

    improved the layout design of tasks

--- src/js/storage.js
+++ src/js/storage.js
@@ -33,12 +33,17 @@ window.addEventListener('load', storage.restoreLocal);
 
 const changeLayout = () => {
     const allTasks = document.querySelectorAll('.task_list_item');
+
     if(window.innerWidth < 700) {
         taskArray.forEach(el => {
             if(el.dueDate !== "") {
                 allTasks.forEach(task => {
-                    if(task.dataset.id === el.id) {
+                    const title = task.querySelector('.item_title').textContent;
+                    if(task.dataset.id === el.id && title.length > 30) {
+                        console.log(title.length)
                         task.style.flexDirection = "column";
+                        task.style.paddingRight = "30px";
+
                     }
                 })
             }
